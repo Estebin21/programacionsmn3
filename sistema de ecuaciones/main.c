521d56771a91367c2324b36a510e9c27baabaacb
Estebin21 <esteban.narvaez.acurio@udla.edu.ec>
2023-06-07 21:47:41 -0500

    cramer

--- sistema de ecuaciones/main.c
+++ sistema de ecuaciones/main.c
@@ -8,12 +8,14 @@ int main() {
     printf("Ingrese los elementos de la matriz 3x3:\n");
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
+            printf("ingresa un termino:\n");
             scanf("%f", &matrix[i][j]);
         }
     }
 
     printf("Ingrese los elementos del vector de términos independientes:\n");
     for (int i = 0; i < 3; i++) {
+        printf("ingresa un termino del vector:\n");
         scanf("%f", &vector[i]);
     }
 
