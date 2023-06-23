8edc086214fd1b134c32021416da133f4659aad0
Estebin21 <130101453+Estebin21@users.noreply.github.com>
2023-06-23 10:06:08 -0500

    Delete examen2 directory

--- examen2/funciones.h
+++
@@ -1,4 +0,0 @@
-int ingreseComponente(char coordenada, int index);
-float calcularVolumen(int cono[][3]);
-void imprimirVolumenArea(float volumen1, float volumen2, int cono1[][3], int cono2[][3]);
-
