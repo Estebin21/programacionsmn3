4c5512f2a50536ecbba31076a34eabcedb7b5b01
Estebin21 <130101453+Estebin21@users.noreply.github.com>
2023-06-23 10:06:15 -0500

    Delete examnrepaso2 directory

--- examnrepaso2/funciones.h
+++
@@ -1,4 +0,0 @@
-int ingreseCedula();
-int obtenerPosicionContacto(int cedula[], int cedulaBuscar);
-void imprimirContactos(char nombre[][50], int telefonos[][2], int indice);
-void imprimirDirecciones(char direcciones[][2][50], int indice);
